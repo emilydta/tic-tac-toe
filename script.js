11bcf8f1c4e205286e1592657090579e350b7153
Emily <emilydta@users.noreply.github.com>
2022-03-05 17:58:25 +1100

    refactor: simpler way to add to array+display

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
   let array = [
-    "p","X","p",
-    "p","p","p",
-    "p","p","p"
+    "","X","",
+    "","","",
+    "","",""
   ]
 
   const gridSquare = document.querySelectorAll(".grid-square");
@@ -12,25 +12,15 @@ const gameBoard = (() => {
 })()
 
 const displayController = (() => {
-  const addArrayToDisplay = (() => {
-    for (let i = 0; i < gameBoard.array.length; i++) {
-      gameBoard.gridSquare[i].innerText = gameBoard.array[i];
-      if (gameBoard.array[i] == "p") {
-        gameBoard.gridSquare[i].innerText = "";
-      }
-    }
-  })()
-
-  const gridSquare = document.querySelector(".grid-squares");
-  gridSquare.addEventListener("click", (e) => {
-      if (e.target.innerText == "") {
-        e.target.innerText = "X"
-      };
-      for (let i = 0; i < 9; i++) {
-        gameBoard.array = [];
-        gameBoard.array.push(`${gameBoard.gridSquare[i].innerText}`)
-        console.log(gameBoard.array);
-      }
-  });
-
+  const addMarkToBoard = (() => {
+    const gridSquare = document.querySelector(".grid-squares");
+    gridSquare.addEventListener("click", (e) => {
+        if (e.target.innerText == "") {
+          e.target.innerText = "X"
+        };
+        for (let i = 0; i < 9; i++) {
+          gameBoard.array.splice(i, 1, `${gameBoard.gridSquare[i].innerText}`)
+        }
+    });
+  })();
 })();
